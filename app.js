ad1ce1a4206321eb74bd985c95baa23d914799af
rose <159682249+Rosa-Mallow@users.noreply.github.com>
2025-04-03 22:05:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const search-button = document.getElementbyID("submitSearch").addEventListener("click", fetchAPI());
-const input-field = document.getElementbyID("searchWord");
-const image = document.getElementbyID("imageContainer");
+const searchButton = document.getElementbyID("submitSearch").addEventListener("click", fetchAPI);
+const inputField = document.getElementbyID("searchWord");
+const imageDisplay = document.getElementbyID("imageContainer");
 
 function fetchAPI() {
-  fetch("https://api.giphy.com/v1/gifs/translate")
-  .then(response => {
-    return response.json();
-  })
+  const myAPIkey = "PqI8GDtWrSfu44r6eA1HmCMGVlCX7Mqt";
+  
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${myAPIkey}&s=${encodeURIComponent(inputField.value)}`)
+  .then(response => response.json();)
   .then(data => {
-    
+    const extractedGIF = data.data.images.original.url;
+    imageDisplay.innerHTML = `<img src="${extractedGIF}" alt="GIF Result">`;
   })
-}//to be changed
+}
